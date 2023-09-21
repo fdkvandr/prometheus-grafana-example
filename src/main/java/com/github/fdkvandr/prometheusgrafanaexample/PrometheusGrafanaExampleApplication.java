9966e3fd463284fa422eba2666e9d1de13e33633
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-09-21 13:44:22 +0300

    feat: initial commit, project configuration, first controller

---
+++ src/main/java/com/github/fdkvandr/prometheusgrafanaexample/PrometheusGrafanaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.fdkvandr.prometheusgrafanaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusGrafanaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusGrafanaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fdkvandr/prometheusgrafanaexample/PrometheusGrafanaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fdkvandr.prometheusgrafanaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGrafanaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
